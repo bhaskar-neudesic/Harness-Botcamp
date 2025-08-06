e1527672ada478dff8e92d55e0692f3cf3097579
bhaskar.sharma <bhaskar.sharma@neudesic.com>
2025-08-07 00:58:11 +0530

    updated storage details

--- Infra/AKS/backend.tf
+++ Infra/AKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestoragebs"
+    storage_account_name = "tfstatestoragebs"  # updated name
     container_name       = "tfstate"
     key                  = "aks-harness.tfstate"
   }
